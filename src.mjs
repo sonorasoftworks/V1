0aad8419cb266397caad6fd384a95bbbaab67bf0
rxfe-a <28RafaelA539@gmail.com>
2024-06-19 00:51:25 -0700

    b

--- src.mjs
+++ src.mjs
@@ -7,11 +7,21 @@ import express from 'express';
 import { hostname } from "node:os";
 import Unblocker from "fetch-behind";
 
+function generateRandomPath(length) {
+    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result = '';
+    for (let i = 0; i < length; i++) {
+      result += chars.charAt(Math.floor(Math.random() * chars.length));
+    }
+    return result;
+  }
+
+const BareKey = generateRandomPath(35)
 const __dirname = process.cwd();
 const httpServer = http.createServer();
 const app = express();
 app.use(express.static('./frontend'));
-const bareServer = createBareServer('/bare/');
+const bareServer = createBareServer(`/${BareKey}/`);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -35,10 +45,13 @@ httpServer.on('upgrade', (req, socket, head) => {
     }
 });
 
+
 httpServer.listen(PORT, () => {
 const reset = "\x1b[0m";
 const fgBlue = "\x1b[34m";
 const fgWhite = "\x1b[37m"; 
 console.log(`${fgBlue}S${fgWhite}onora${reset}`);
-console.log(`${fgBlue}v1`)
+console.log(`${fgBlue}v1${reset}`);
+console.log(`Running on port ${PORT}`);
+console.log(`Bare Key : ${BareKey}`)
 });
